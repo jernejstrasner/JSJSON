65bdfe1a2dfa7025dcfcf7799ddf6732308db32f
Jernej Strasner <jernej.strasner@gmail.com>
2014-12-23 12:13:05 +0100

    Initial Commit

---
+++ JSJSON/JSJSON.h
@@ -0,0 +1,19 @@
+//
+//  JSJSON.h
+//  JSJSON
+//
+//  Created by Jernej Strasner on 12/23/14.
+//  Copyright (c) 2014 Jernej Strasner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSJSON.
+FOUNDATION_EXPORT double JSJSONVersionNumber;
+
+//! Project version string for JSJSON.
+FOUNDATION_EXPORT const unsigned char JSJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSJSON/PublicHeader.h>
+
+
